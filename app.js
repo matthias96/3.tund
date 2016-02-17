79918056dd13f1ae7fd73edb92102a7a642098e1
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2016-02-17 09:26:11 +0200

    added routes

--- app.js
+++ app.js
@@ -8,17 +8,54 @@
       }
       Moosipurk.instance = this; // this viitab moosipurgile
       //console.log(this);
+      this.routes = Moosipurk.routes;
       // KÕIK MUUTUJAD, mis on üldised ja muudetavad
+      this.currentRoute = null; //hoian meeles mis Lehel olen(home-view)
+
       this.click_count= 0;
       //panen rakenduse tööle
 
       this.init();
     };
+    //kirjeldatud kõik lehed
+    Moosipurk.routes= {
+      "home-view":{
+        render: function(){
+          // käivitan siis kui jõuan lehele
+          console.log('JS avalehel');
+
+          //kui jõuan avalehele siis käivitub timer, mis hakkab trükkima kulunud sekundeid
+          // divi sisse #counter
+          // hakkab 0-st
+        }
+      },
+      "list-view":{
+        render: function(){
+          console.log('JS loendil');
+        }
+      },
+      "manage-view":{
+        render: function(){
+          console.log('JS haldusel');
+        }
+      }
+    };
+
     Moosipurk.prototype = {
       //kõik moosipurgi funktsioonid tulevad siia sisse
       init: function(){
         console.log('rakendus käivitus');
         // siia tuleb esialgne loogika
+        window.addEventListener('hashchange', this.routeChange.bind(this));
+        // vaatan mis lehel olen
+        console.log(window.location.hash);
+        if(!window.location.hash){
+          window.location.hash="#home-view";
+        }else{
+          //hash oli olemas, käivitan routeChange funktsiooni
+          this.routeChange();
+
+        }
         // hakka kuulama hiireklõpse
         this.bindMouseEvents();
       },
@@ -29,6 +66,23 @@
         //console.log(event);
         this.click_count++;
         console.log(this.click_count);
+      },
+      routeChange: function(){
+
+
+        this.currentRoute = window.location.hash.slice(1);
+        // kas leht on olemas
+        if(this.routes[this.currentRoute]){
+          //da
+          console.log('>>>>>>>' + this.currentRoute);
+          //käivitan selle lehe jaoks ettenähtud javascripti
+          this.routes[this.currentRoute].render();
+        }else{
+          // 404?
+
+          console.log('404');
+          window.location.hash = 'home-view';
+        }
       }
     };
     window.onload=function (){
